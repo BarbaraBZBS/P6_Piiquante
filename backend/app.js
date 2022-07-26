25fee3ad364666214dec4041533dd3355f4f174e
Bazabas Barbara <barbara-bazabas@live.fr>
2022-07-26 16:08:52 +0200

    added multer img files handler

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
+const path = require('path');
+
 mongoose.connect('mongodb+srv://Allison:allipass@cluster0.mekwpjz.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
@@ -22,6 +24,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
